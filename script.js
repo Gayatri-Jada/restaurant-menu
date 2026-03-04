c8f32481ec21a5dd00c9cbd78e22a339d1d9056a
Gayatri-Jada <gayatrijada03@gmail.com>
2026-03-04 11:28:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function payNow() {
     document.getElementById("orderId").innerText =
         "Your Order ID: " + generateOrderId();
 }
+
+function generateOrderId() {
+    return "ORD" + Math.floor(Math.random() * 100000);
+}
+
+function payNow() {
+    document.querySelector(".menu-container").style.display = "none";
+    document.querySelector(".total-section").style.display = "none";
+
+    document.getElementById("paymentSuccess").style.display = "block";
+    document.getElementById("orderId").innerText =
+        "Your Order ID: " + generateOrderId();
+}
